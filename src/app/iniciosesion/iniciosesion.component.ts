e6166729e900e91ab28c1038200fcc87eb1e9e10
Carlo0s-HernandezZ <2123100490@soy.utj.edu.mx>
2025-06-24 19:45:36 -0600

    mejoras

--- src/app/iniciosesion/iniciosesion.component.ts
+++ src/app/iniciosesion/iniciosesion.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { IniciodesesionService } from '../servicios/iniciodesesion.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-iniciosesion',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './iniciosesion.component.html',
-  styleUrl: './iniciosesion.component.css'
+  styleUrl: './iniciosesion.component.css',
 })
 export class IniciosesionComponent {
+
+  usuario={
+    email:"",
+    password:""
+  }
+
+  constructor(private servicioIniciosesion:IniciodesesionService
+              ,private router:Router
+  ){}
+  
+  login(){
+    this.servicioIniciosesion.login(this.usuario).subscribe(
+      res=>{
+        localStorage.setItem("token", res.jwtoken);
+        alert(res.msj);
+        this.router.navigate(['/inicio']);
+      },
+      err=>{
+        alert(err);
+      }
+    );
+  }
 
 }
--- src/app/servicios/iniciodesesion.service.ts
+++ src/app/servicios/iniciodesesion.service.ts
@@ -1,26 +1,46 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Token } from '@angular/compiler';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IniciodesesionService {
 
-  constructor(private http:HttpClient) { }
 
-  login(){
+  private url = "http://localhost:3000/clientes";
+  constructor(private http:HttpClient, private router:Router) { }
 
+  login(usuario:object){
+    return this.http.post<any>(this.url,usuario);
   }
 
   cerrarSesion(){
 
+    //borrar una variable de localstorage
+    //localStorage.removeItem("token");
+
+    //borrar todas las variable de localstorage
+    localStorage.clear();
+    this.router.navigate(['/iniciosesion']);//validar la ruta Carlos no sea wey-------------------------------
+
   }
 
-  eslogeado(){
+  eslogeado():boolean{
+    /*     if(localStorage.getItem("token")!=null){ //localstora es una galleta, si existe se inicio sesion, si no no se inicio sesion, la fucion revisa que exista
+return true;
+    }else{//
+      return false;
+    } */ //es lo mismo que lo de abajo pero con funcion enangular
+
+    return !!localStorage.getItem("token");// el !! es como si se usara un if else
 
   }
 
   tuperfil(){
+    //saber cual es el puest del empleado
+    //return localStorage.getItem("perfil");//guarda el puesto del empleado
 
   }
 }
